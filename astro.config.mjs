a85d28ee9a3b1a3d305e338f93445b0ff936496c
goranrad <goranrad@gmail.com>
2025-06-12 14:06:40 +0200

    Update site URL to custom domain (elastrio.com)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://elastrio.github.io',
+  site: 'https://elastrio.com/',
   base: '/',
 });
